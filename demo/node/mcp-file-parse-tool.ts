64a38d8219c219661e55950a8f7e4d086d41df37
Alex Yang <himself65@outlook.com>
2025-04-01 02:20:58 -0700

    feat: add example

--- demo/node/mcp-file-parse-tool.ts
+++ demo/node/mcp-file-parse-tool.ts
@@ -1,30 +1,52 @@
-import { mcpTool } from 'fluere/interrupter/mcp'
-import { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
-import { z } from 'zod'
-import { fileParseWorkflow } from '../workflows/file-parse-agent.js'
-import { createWorkflow, workflowEvent } from 'fluere'
+import { mcpTool } from "fluere/interrupter/mcp";
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { z } from "zod";
+import { fileParseWorkflow } from "../workflows/file-parse-agent.js";
+import { createWorkflow, workflowEvent } from "fluere";
+import { consume } from "fluere/stream";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
 
 const server = new McpServer({
-  name: 'Demo',
-  version: '1.0.0'
-})
+  name: "Demo",
+  version: "1.0.0",
+});
 
 const startEvent = workflowEvent<{
-  filePath: string
-}>()
+  filePath: string;
+}>();
 const stopEvent = workflowEvent<{
-  content: ({ type: 'text', text: string })[]
-}>()
+  content: { type: "text"; text: string }[];
+}>();
 
 const wrappedWorkflow = createWorkflow({
   startEvent,
-  stopEvent
-})
+  stopEvent,
+});
 
-wrappedWorkflow.handle([startEvent],
-  async ({ data: { filePath } }) => {fileParseWorkflow.run(filePath)}
-)
+wrappedWorkflow.handle([startEvent], async ({ data: { filePath } }) => {
+  const { stream, sendEvent } = fileParseWorkflow.createContext();
+  sendEvent(fileParseWorkflow.startEvent(filePath));
+  await consume(stream, fileParseWorkflow.stopEvent);
+  return stopEvent({
+    content: [
+      {
+        type: "text",
+        text: fileParseWorkflow.getStore().output,
+      },
+    ],
+  });
+});
 
-server.tool('1', {
-  filePath: z.string()
-}, mcpTool(wrappedWorkflow))
+server.tool(
+  "list directory",
+  {
+    filePath: z.string(),
+  },
+  mcpTool(wrappedWorkflow),
+);
+
+const transport = new StdioServerTransport();
+
+server.connect(transport).then(() => {
+  console.log("Connected");
+});
--- packages/fluere/src/interrupter/mcp.ts
+++ packages/fluere/src/interrupter/mcp.ts
@@ -1,22 +1,33 @@
-import { z, type ZodRawShape, type ZodTypeAny } from 'zod'
-import type { Workflow } from 'fluere'
-import { promiseHandler } from './promise'
-import type {
-  RequestHandlerExtra
-} from '@modelcontextprotocol/sdk/shared/protocol.js'
-import type { CallToolResult } from '@modelcontextprotocol/sdk/types.js'
+import { AsyncLocalStorage } from "node:async_hooks";
+import { z, type ZodRawShape, type ZodTypeAny } from "zod";
+import type { Workflow } from "../core";
+import { promiseHandler } from "./promise";
+import type { RequestHandlerExtra } from "@modelcontextprotocol/sdk/shared/protocol.js";
+import type { CallToolResult } from "@modelcontextprotocol/sdk/types.js";
 
-const requestHandlerExtraAsyncLocalStorage = new AsyncLocalStorage()
+const requestHandlerExtraAsyncLocalStorage = new AsyncLocalStorage();
 
-export function mcpTool <
+export const getReqHandlerExtra = () => {
+  const extra = requestHandlerExtraAsyncLocalStorage.getStore();
+  if (!extra) {
+    throw new Error("Request handler extra not set");
+  }
+  return extra;
+};
+
+export function mcpTool<
   Args extends ZodRawShape,
   Start extends z.objectOutputType<Args, ZodTypeAny>,
-  Stop extends CallToolResult
+  Stop extends CallToolResult,
 >(
-  workflow: Workflow<Start, Stop>
-): (args: Start, extra: RequestHandlerExtra) => CallToolResult | Promise<CallToolResult> {
-  return async (args, extra) => {
-    const { data } = await promiseHandler(() => requestHandlerExtraAsyncLocalStorage.run(extra, () => workflow.run(args)))
-    return data
-  }
+  workflow: Workflow<Start, Stop>,
+): (
+  args: Start,
+  extra: RequestHandlerExtra,
+) => CallToolResult | Promise<CallToolResult> {
+  return async (args, extra) =>
+    requestHandlerExtraAsyncLocalStorage.run(extra, async () => {
+      const { data } = await promiseHandler(workflow, args);
+      return data;
+    });
 }
