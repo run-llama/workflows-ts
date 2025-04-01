f9f27e2bf0f407ca9fca9233448bf82b86f08ed2
Alex Yang <himself65@outlook.com>
2025-04-01 12:04:55 -0700

    feat: add browser demo

--- packages/fluere/src/interrupter/promise.ts
+++ packages/fluere/src/interrupter/promise.ts
@@ -7,10 +7,18 @@ import { finalize } from "../stream";
  * Resolves when the workflow reads the stop event.
  *  reject if the workflow throws an error or times out.
  */
+export async function promiseHandler<Start extends void, Stop>(
+  workflow: Workflow<Start, Stop>,
+  start?: void | WorkflowEventData<Start>,
+): Promise<WorkflowEventData<Stop>>
 export async function promiseHandler<Start, Stop>(
   workflow: Workflow<Start, Stop>,
   start: Start | WorkflowEventData<Start>,
-): Promise<WorkflowEventData<Stop>> {
+): Promise<WorkflowEventData<Stop>>
+export async function promiseHandler(
+  workflow: Workflow<any, any>,
+  start?: any | WorkflowEventData<any>,
+): Promise<WorkflowEventData<any>> {
   const stream = finalize(workflow, start);
   for await (const event of stream) {
     if (workflow.stopEvent.include(event)) {
