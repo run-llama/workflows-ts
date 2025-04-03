b526d266b43e62a0eb2c9986cde9001e32ed9df2
Alex Yang <himself65@outlook.com>
2025-04-03 01:43:07 -0700

    test: fix tsconfig paths

--- packages/fluere/vitest.config.ts
+++ packages/fluere/vitest.config.ts
@@ -4,7 +4,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   plugins: [
     tsconfigPaths({
-      projects: ["./tsconfig.test.json"],
+      projects: ["./tsconfig.json"],
     }),
   ],
 });
