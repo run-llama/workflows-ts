56ce225634b8d4b1e4313f9727761bb3a6cb1032
Alex Yang <himself65@outlook.com>
2025-04-05 23:12:09 -0700

    chore: add lint-staged and husky for pre-commit hooks

---
+++ lint-staged.config.js
@@ -0,0 +1,8 @@
+/**
+ * @filename: lint-staged.config.js
+ * @type {import('lint-staged').Configuration}
+ */
+export default {
+  "*.{js,jsx,ts,tsx}": ["prettier --write"],
+  "*.{json,md,yml}": ["prettier --write"],
+};
