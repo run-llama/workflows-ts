b49aac8605c6f786cccdbd43bbba81cf2889691f
Alex Yang <himself65@outlook.com>
2025-04-19 14:39:53 -0700

    feat: update example

--- demo/node/basic.ts
+++ demo/node/basic.ts
@@ -1,12 +1,10 @@
 import { createWorkflow, workflowEvent, getContext } from "@llama-flow/core";
-import { withTraceEvents } from "@llama-flow/core/middleware/trace-events";
 import { pipeline } from "node:stream/promises";
 import { collect } from "@llama-flow/core/stream/consumer";
 import { until } from "@llama-flow/core/stream/until";
 import { filter } from "@llama-flow/core/stream/filter";
 
-////////// define the workflow
-
+//#region define workflow events
 const startEvent = workflowEvent<string>();
 const branchAEvent = workflowEvent<string>();
 const branchBEvent = workflowEvent<string>();
@@ -14,10 +12,10 @@ const branchCEvent = workflowEvent<string>();
 const branchCompleteEvent = workflowEvent<string>();
 const allCompleteEvent = workflowEvent<string>();
 const stopEvent = workflowEvent<string>();
+//#endregion
 
-const workflow = withTraceEvents(createWorkflow());
-
-// handle the start event
+//#region defines workflow
+const workflow = createWorkflow();
 workflow.handle([startEvent], async () => {
   // emit 3 different events, handled separately
   const { sendEvent, stream } = getContext();
@@ -39,36 +37,29 @@ workflow.handle([startEvent], async () => {
   return allCompleteEvent.with(results.map((e) => e.data).join(", "));
 });
 
-// handle branch A
 workflow.handle([branchAEvent], (branchA) => {
   return branchCompleteEvent.with(branchA.data);
 });
 
-// handle branch B
 workflow.handle([branchBEvent], (branchB) => {
   return branchCompleteEvent.with(branchB.data);
 });
 
-// handle branch C
 workflow.handle([branchCEvent], (branchC) => {
   return branchCompleteEvent.with(branchC.data);
 });
 
-// handle the collected results
 workflow.handle([allCompleteEvent], (allComplete) => {
   return stopEvent.with(allComplete.data);
 });
 
-////////// run the workflow
+//#endregion
 
-// Create a workflow context and send the initial event
 const { stream, sendEvent } = workflow.createContext();
-sendEvent(startEvent.with("I am some data"));
+sendEvent(startEvent.with("initial data"));
 
-// Process the stream to get the result
 pipeline(stream, async function (source) {
   for await (const event of source) {
-    console.log(`Event: ${event.data}`);
     if (stopEvent.include(event)) {
       return `Result: ${event.data}`;
     }
