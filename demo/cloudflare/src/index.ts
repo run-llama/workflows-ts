ddd1248806462e1159a84b17c7cee01dc574c9f5
Alex Yang <himself65@outlook.com>
2025-04-23 16:43:30 -0700

    fix: browser demo

--- demo/cloudflare/src/index.ts
+++ demo/cloudflare/src/index.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { createWorkflow, workflowEvent } from "@llama-flow/core";
-import { createHonoHandler } from "@llama-flow/core/interrupter/hono";
+import { createHonoHandler } from "@llama-flow/core/hono";
 import { html } from "hono/html";
 
 const app = new Hono();
