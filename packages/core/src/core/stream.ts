2a18aca125dfd84bf1e8c9438080baba5481fd45
Alex Yang <himself65@outlook.com>
2025-04-25 18:37:35 -0700

    feat: `stream.on` API (#89)

---
+++ packages/core/src/core/stream.ts
@@ -0,0 +1,78 @@
+import { type WorkflowEvent, type WorkflowEventData } from "./event";
+import type { Subscribable } from "./utils";
+
+export class WorkflowStream<Event extends WorkflowEvent<any>> {
+  #subscribable: Subscribable<[event: WorkflowEventData<any>], void>;
+  #stream: ReadableStream<ReturnType<Event["with"]>>;
+
+  on<T extends Event>(
+    event: T,
+    handler: (event: ReturnType<T["with"]>) => void,
+  ): () => void {
+    return this.#subscribable.subscribe((ev) => {
+      if (event.include(ev)) {
+        handler(ev as ReturnType<T["with"]>);
+      }
+    });
+  }
+
+  constructor(
+    subscribable: Subscribable<[event: WorkflowEventData<any>], void>,
+    stream: ReadableStream<ReturnType<Event["with"]>>,
+  ) {
+    this.#subscribable = subscribable;
+    this.#stream = stream;
+  }
+
+  get locked() {
+    return this.#stream.locked;
+  }
+
+  [Symbol.asyncIterator](): ReadableStreamAsyncIterator<
+    ReturnType<Event["with"]>
+  > {
+    return this.#stream[Symbol.asyncIterator]();
+  }
+
+  cancel(reason?: any): Promise<void> {
+    return this.#stream.cancel(reason);
+  }
+
+  // make type compatible with Web ReadableStream API
+  getReader(options: { mode: "byob" }): ReadableStreamBYOBReader;
+  getReader(): ReadableStreamDefaultReader<ReturnType<Event["with"]>>;
+  getReader(
+    options?: ReadableStreamGetReaderOptions,
+  ): ReadableStreamReader<ReturnType<Event["with"]>>;
+  getReader(): any {
+    return this.#stream.getReader();
+  }
+
+  pipeThrough<T = WorkflowEventData<any>>(
+    transform: ReadableWritablePair<T, ReturnType<Event["with"]>>,
+    options?: StreamPipeOptions,
+  ): ReadableStream<T> {
+    return this.#stream.pipeThrough(transform, options);
+  }
+
+  pipeTo(
+    destination: WritableStream<ReturnType<Event["with"]>>,
+    options?: StreamPipeOptions,
+  ): Promise<void> {
+    return this.#stream.pipeTo(destination, options);
+  }
+
+  tee(): [WorkflowStream<Event>, WorkflowStream<Event>] {
+    const [l, r] = this.#stream.tee();
+    return [
+      new WorkflowStream(this.#subscribable, l),
+      new WorkflowStream(this.#subscribable, r),
+    ];
+  }
+
+  values(
+    options?: ReadableStreamIteratorOptions,
+  ): ReadableStreamAsyncIterator<ReturnType<Event["with"]>> {
+    return this.#stream.values(options);
+  }
+}
---
+++ packages/core/tests/core/listener.spec.ts
@@ -0,0 +1,25 @@
+import { describe, expect, test, vi } from "vitest";
+import { createWorkflow, getContext, workflowEvent } from "@llama-flow/core";
+
+describe("workflow listener api", () => {
+  test("can listen message event", () => {
+    const workflow = createWorkflow();
+    const startEvent = workflowEvent();
+    const messageEvent = workflowEvent<string>({});
+    workflow.handle([startEvent], () => {
+      const { sendEvent } = getContext();
+      sendEvent(messageEvent.with("Hello World"));
+    });
+
+    const { stream, sendEvent } = workflow.createContext();
+    const callback = vi.fn((event: ReturnType<typeof messageEvent.with>) => {
+      expect(event.data).toBe("Hello World");
+    });
+    const unsubscribe = stream.on(messageEvent, callback);
+    sendEvent(startEvent.with());
+    expect(callback).toHaveBeenCalledTimes(1);
+    unsubscribe();
+    sendEvent(startEvent.with());
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+});
