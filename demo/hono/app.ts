3720867b09e303cc6dd0671d471238a50dbe537d
Alex Yang <himself65@outlook.com>
2025-04-30 16:14:30 -0700

    fix: hono example

--- demo/hono/app.ts
+++ demo/hono/app.ts
@@ -6,6 +6,8 @@ import {
   startEvent,
   stopEvent,
 } from "../workflows/tool-call-agent.js";
+import { until } from "@llama-flow/core/stream/until";
+import { collect } from "@llama-flow/core/stream/consumer";
 
 const app = new Hono();
 
@@ -17,6 +19,53 @@ app.post(
     stopEvent,
   ),
 );
+
+const serializableMemoryMap = new Map<string, any>();
+
+app.post("/human-in-the-loop", async (ctx) => {
+  const { workflow, stopEvent, startEvent, humanInteractionEvent } =
+    await import("../workflows/human-in-the-loop");
+  const json = await ctx.req.json();
+  let context: ReturnType<typeof workflow.createContext>;
+  if (json.requestId) {
+    const data = json.data;
+    const serializable = serializableMemoryMap.get(json.requestId);
+    context = workflow.resume(data, serializable);
+  } else {
+    context = workflow.createContext();
+    context.sendEvent(startEvent.with(json.data));
+  }
+
+  const { onRequest, stream } = context;
+  return new Promise<Response>((resolve) => {
+    // listen to human interaction
+    onRequest(async (event, reason) => {
+      if (humanInteractionEvent === event) {
+        context.snapshot().then(([re, sd]) => {
+          const requestId = crypto.randomUUID();
+          serializableMemoryMap.set(requestId, sd);
+          resolve(
+            Response.json({
+              requestId: requestId,
+              reason: reason,
+              data: re.map((r) =>
+                r === humanInteractionEvent
+                  ? "request human in the loop"
+                  : "UNKNOWN",
+              ),
+            }),
+          );
+        });
+      }
+    });
+
+    // consume stream
+    collect(until(stream, stopEvent)).then((events) => {
+      const stopEvent = events.at(-1)!;
+      resolve(Response.json(stopEvent.data));
+    });
+  });
+});
 
 serve(app, ({ port }) => {
   console.log(`Server started at http://localhost:${port}`);
