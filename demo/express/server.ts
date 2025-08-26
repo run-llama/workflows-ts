d6e0b413e2516018a76281f4e1241b8e209cab52
Marcus Schiesser <marcus.schiesser@googlemail.com>
2025-08-26 13:33:44 +0800

    chore: use on for getting human response event (#146)

--- demo/express/server.ts
+++ demo/express/server.ts
@@ -170,7 +170,6 @@ workflow.handle([toolCallEvent], async (context, event) => {
   try {
     if (toolCall.function.name.startsWith("human_")) {
       state.humanToolId = toolCall.id;
-      // sendEvent(request(humanResponseEvent));
       sendEvent(humanRequestEvent.with());
     } else {
       const toolResponse = await callTool(toolCall);
@@ -218,26 +217,19 @@ app.post("/workflow/start", async (req, res) => {
       humanToolId: null,
     });
 
-    const stream = context.stream.pipeThrough(
-      new TransformStream({
-        async transform(event, controller) {
-          if (humanRequestEvent.include(event)) {
-            // workflow is interrupted by a human request, we need to save the current state
-            const [_, snapshotData] = await context.snapshot();
-            snapshots.set(requestId, snapshotData);
-            res.json({
-              type: "waiting_for_human",
-              requestId,
-              messages: context.state.messages,
-            });
-          }
-          controller.enqueue(event);
-        },
-      }),
-    );
+    context.stream.on(humanRequestEvent, async () => {
+      // workflow is interrupted by a human request, we need to save the current state
+      const [_, snapshotData] = await context.snapshot();
+      snapshots.set(requestId, snapshotData);
+      res.json({
+        type: "waiting_for_human",
+        requestId,
+        messages: context.state.messages,
+      });
+    });
 
     context.sendEvent(userInputEvent.with({ messages }));
-    await stream.until(finalResponseEvent).toArray();
+    await context.stream.until(finalResponseEvent).toArray();
 
     res.json({
       type: "completed",
