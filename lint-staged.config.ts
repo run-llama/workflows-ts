ef29293f98cd7e23e44b62ace8bda502753989e1
Marcus Schiesser <mail@marcusschiesser.de>
2025-09-08 10:36:13 +0800

    chore: add biome

--- lint-staged.config.ts
+++ lint-staged.config.ts
@@ -1,6 +1,5 @@
 import type { Configuration } from "lint-staged";
 
 export default {
-  "*.{js,jsx,ts,tsx}": ["prettier --write"],
-  "*.{json,md,yml}": ["prettier --write"],
+	"*": ["biome check --write"],
 } satisfies Configuration;
