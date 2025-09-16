39fa21270f593fb04dd39f50b5ed5982461f8075
Thuc Pham <thuc@lingble.com>
2025-09-16 16:01:25 +0700

    fix format

--- demo/browser/src/App.tsx
+++ demo/browser/src/App.tsx
@@ -10,9 +10,9 @@ const stopEvent = workflowEvent<string>();
 const workflow = createWorkflow();
 
 workflow.handle([startEvent], (context) => {
-	setTimeout(() => {
-		context.sendEvent(stopEvent.with("Hello, World!"));
-	}, 1000);
+  setTimeout(() => {
+    context.sendEvent(stopEvent.with("Hello, World!"));
+  }, 1000);
 });
 
 const context = workflow.createContext();
@@ -21,27 +21,29 @@ context.sendEvent(startEvent.with());
 const events = await context.stream.until(stopEvent).toArray();
 
 function App() {
-	return (
-		<>
-			<div>
-				<a href="https://llamaindex.ai" target="_blank" rel="noopener">
-					<img src={llamaindexLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noopener">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>React + Llamaindex Flow</h1>
-			<div className="card">
-				<p>
-					<Suspense fallback="Loading...">{events.map(({ data }) => data)}</Suspense>
-				</p>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div>
+        <a href="https://llamaindex.ai" target="_blank" rel="noopener">
+          <img src={llamaindexLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank" rel="noopener">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>React + Llamaindex Flow</h1>
+      <div className="card">
+        <p>
+          <Suspense fallback="Loading...">
+            {events.map(({ data }) => data)}
+          </Suspense>
+        </p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default App;
