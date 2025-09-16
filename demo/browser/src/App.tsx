6686c655aabcc99fb1f4b41fa16412be23ee7d37
Thuc Pham <thuc@lingble.com>
2025-09-16 15:09:04 +0700

    use context.stream.until

--- demo/browser/src/App.tsx
+++ demo/browser/src/App.tsx
@@ -1,12 +1,7 @@
 import llamaindexLogo from "/llamaindex.svg";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import {
-  createWorkflow,
-  getContext,
-  workflowEvent,
-} from "@llamaindex/workflow-core";
-import { runWorkflow } from "@llamaindex/workflow-core/stream/run";
+import { createWorkflow, workflowEvent } from "@llamaindex/workflow-core";
 import { Suspense } from "react";
 
 const startEvent = workflowEvent();
@@ -14,39 +9,39 @@ const stopEvent = workflowEvent<string>();
 
 const workflow = createWorkflow();
 
-workflow.handle([startEvent], () => {
-  setTimeout(() => {
-    const context = getContext();
-    context.sendEvent(stopEvent.with("Hello, World!"));
-  }, 1000);
+workflow.handle([startEvent], (context) => {
+	setTimeout(() => {
+		context.sendEvent(stopEvent.with("Hello, World!"));
+	}, 1000);
 });
 
-const promise = runWorkflow(workflow, startEvent.with(), stopEvent);
+const context = workflow.createContext();
+context.sendEvent(startEvent.with());
+
+const events = await context.stream.until(stopEvent).toArray();
 
 function App() {
-  return (
-    <>
-      <div>
-        <a href="https://llamaindex.ai" target="_blank" rel="noopener">
-          <img src={llamaindexLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>React + Llamaindex Flow</h1>
-      <div className="card">
-        <p>
-          <Suspense fallback="Loading...">
-            {promise.then(({ data }) => data)}
-          </Suspense>
-        </p>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div>
+				<a href="https://llamaindex.ai" target="_blank" rel="noopener">
+					<img src={llamaindexLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank" rel="noopener">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>React + Llamaindex Flow</h1>
+			<div className="card">
+				<p>
+					<Suspense fallback="Loading...">{events.map(({ data }) => data)}</Suspense>
+				</p>
+				<p>
+					Edit <code>src/App.tsx</code> and save to test HMR
+				</p>
+			</div>
+		</>
+	);
 }
 
 export default App;
